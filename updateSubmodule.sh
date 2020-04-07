fbb3201fd32da8f9bf194d0fa39db76dec34b83b
shengj <shengj@rd.netease.com>
2020-04-07 17:41:26 +0800

    rebuilding site Tue Apr  7 17:41:24 CST 2020

---
+++ updateSubmodule.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "\033[0;32mUpdating submodules...\033[0m"
+
+cd public
+git pull
+cd ..
+cd themes/beautifulhugo
+git pull
+
+# Come Back up to the Project Root
+cd ../..
+./deploy.sh
